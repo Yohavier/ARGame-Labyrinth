06364354b94c812b425a9fab44f316e0c5eff691
Yohavier <40822700+Yohavier@users.noreply.github.com>
2020-06-15 16:08:27 +0200

    Trace V.1
    - Project cleanup
    - First tryout of traces

---
+++ DVL/Assets/Scripts/Player/TraceEffect.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TraceEffect : MonoBehaviour
+{
+    void Start()
+    {
+        transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+        StartCoroutine(Expand());
+    }
+
+    private IEnumerator Expand()
+    {
+        while (transform.localScale.x < 0.1f)
+        {
+            transform.localScale += new Vector3(0.005f, 0, 0.005f);
+            yield return new WaitForSeconds(0.01f);
+        }
+        Destroy(this.gameObject);
+    }
+
+    private void OnDestroy()
+    {
+        StopAllCoroutines();
+    }
+}
--- DVL/Assets/Scripts/Player/WalkingTraces.cs
+++ DVL/Assets/Scripts/Player/WalkingTraces.cs
@@ -5,29 +5,22 @@ using UnityEngine.UIElements;
 
 public class WalkingTraces : MonoBehaviour
 {
-    [SerializeField] ParticleSystem FootstepTrace;
+    [SerializeField] GameObject FootstepTrace;
 
     public void SpawnParticlesystem(Tile tile)
-    {
+    {       
         if (VisibilityCheck(tile))
         {
-            ParticleSystem trace = Instantiate(FootstepTrace);
-            trace.transform.SetParent(tile.transform);
-            trace.transform.localPosition = Vector3.zero;
-            trace.transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
-            Destroy(trace.gameObject, trace.startLifetime);
-        }       
+            GameObject trace = Instantiate(FootstepTrace);
+            trace.transform.localPosition = tile.transform.localPosition;
+        } 
     }
 
     private bool VisibilityCheck(Tile tile)
     {
-        if (tile.isInFOW)
+        if (!tile.isInFOW)
             return false;
-
-        DetectDirectNeighbours n = new DetectDirectNeighbours();
-        if (n.Detect(tile).Contains(LocalGameManager.instance.activePlayer.GetComponent<Player>().positionTile))
-            return true;
         else
-            return false;
+            return true;
     }
 }
