1b5ea48f3d554e17628bd35a9de5c0de7378ec74
Yohavier <40822700+Yohavier@users.noreply.github.com>
2020-06-15 19:22:49 +0200

    TraceUpdate
    - Jeder spieler sieht jetzt nur traces in seiner Umgebung
    - Entfernungsvektor bestimmt entfernung nicht tiles

--- DVL/Assets/Scripts/Player/WalkingTraces.cs
+++ DVL/Assets/Scripts/Player/WalkingTraces.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -8,19 +9,26 @@ public class WalkingTraces : MonoBehaviour
     [SerializeField] GameObject FootstepTrace;
 
     public void SpawnParticlesystem(Tile tile)
-    {       
-        if (VisibilityCheck(tile))
-        {
-            GameObject trace = Instantiate(FootstepTrace);
-            trace.transform.localPosition = tile.transform.localPosition;
-        } 
-    }
-
-    private bool VisibilityCheck(Tile tile)
     {
-        if (!tile.isInFOW)
-            return false;
-        else
-            return true;
+        if (tile != null)
+        {
+            if (LocalGameManager.instance.activePlayer != null)
+            {
+                if (!tile.isInFOW)
+                    return;
+
+                Tile A = LocalGameManager.instance.activePlayer.GetComponent<Player>().positionTile;
+                Tile B = tile;
+
+                float distance = (A.transform.position - B.transform.position).sqrMagnitude;
+                Debug.Log(distance);
+
+                if (distance < 0.1)
+                {
+                    GameObject trace = Instantiate(FootstepTrace);
+                    trace.transform.localPosition = tile.transform.localPosition;
+                }
+            }
+        }
     }
 }
