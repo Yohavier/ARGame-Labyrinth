d0d2033f7a80ed2f66f32f7ba4402b16e8dc2432
Yohavier <40822700+Yohavier@users.noreply.github.com>
2020-06-24 20:40:43 +0200

    Make escape Capsule visible

--- DVL/Assets/Scripts/Objects/EscapeCapsule.cs
+++ DVL/Assets/Scripts/Objects/EscapeCapsule.cs
@@ -4,8 +4,21 @@ using UnityEngine;
 
 public class EscapeCapsule : MonoBehaviour
 {
+    MeshRenderer[] meshes;
+    private void Start()
+    {
+        meshes = GetComponentsInChildren<MeshRenderer>();
+    }
     public void DisplayProgress()
     {
         GameManager.instance.CheckWinConditionCrew();
+    }
+
+    private void Update()
+    {
+        foreach(MeshRenderer mesh in meshes)
+        {
+            mesh.material.color = Color.green;
+        }
     }
 }
