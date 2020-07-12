f20a80a2573f43b9cb21c337dfa1e3b30de39901
Yohavier <40822700+Yohavier@users.noreply.github.com>
2020-07-12 18:23:31 +0200

    Lobby Info for Character selection

--- DVL/Assets/Scripts/Menu/CharInfo.cs
+++ DVL/Assets/Scripts/Menu/CharInfo.cs
@@ -5,8 +5,57 @@ using UnityEngine.UI;
 
 public class CharInfo : MonoBehaviour
 {
+    public Text infoText;
+    public Text roleText;
+    public Text movementText;
+    public Text tracetadiusText;
+    public Text visiontadiusText;
+    public Text repairSpeedText;
+    public Text healthText;
+
     public void DisplayNewInfo(SO_PlayerClass role)
     {
-        GetComponentInChildren<Text>().text = role.roleInfoText;
+        SetInfoText(role.roleInfoText);
+        SetRoleText(role.roleName);
+        SetMovementText(role.diceModificator.ToString());
+        SetTraceradiusText(role.footstepDetectionRadius.ToString());
+        SetVisionradiusText(role.fogOfWarRadius.ToString());
+        SetRepairSpeedText(role.repairSpeed);
+        SetHealthText(role.maxDeathTurnCounter);
+    }
+
+    private void SetInfoText(string txt)
+    {
+        infoText.text = txt;
+    }
+    private void SetRoleText(string txt)
+    {
+        roleText.text = txt;
+    }
+    private void SetMovementText(string txt)
+    {
+        movementText.text = "Move Buff: " + txt;
+    }
+    private void SetTraceradiusText(string txt)
+    {
+        tracetadiusText.text = "Traceradius: " + txt;
+    }
+    private void SetVisionradiusText(string txt)
+    {
+        visiontadiusText.text = "Visionradius: " + txt;
+    }
+    private void SetRepairSpeedText(int txt)
+    {
+        if (txt == 0)
+            repairSpeedText.text = "";
+        else
+            repairSpeedText.text = "Repairspeed: " + txt.ToString();
+    }
+    private void SetHealthText(int txt)
+    {
+        if (txt > 50)
+            healthText.text = "Health: Immortal";
+        else
+            healthText.text = "Health: " + txt.ToString();
     }
 }
