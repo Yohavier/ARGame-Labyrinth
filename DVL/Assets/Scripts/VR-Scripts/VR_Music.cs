3e7d9e04de857fb7674c11b32514f50553747d69
Yohavier <40822700+Yohavier@users.noreply.github.com>
2020-09-18 10:20:50 +0200

    doppelter boden

---
+++ DVL/Assets/Scripts/VR-Scripts/VR_Music.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VR_Music : MonoBehaviour
+{
+    private void Start()
+    {
+        AkSoundEngine.SetState("GameState", "InGame");
+        AkSoundEngine.SetRTPCValue("intensity", 75);
+    }
+}
